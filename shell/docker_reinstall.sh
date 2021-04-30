281b06c8c5b929e312725971f59caf31c800f165
uniqs <uniqs@163.com>
2021-04-30 11:22:10 +0800

    ss

--- shell/docker_reinstall.sh
+++ shell/docker_reinstall.sh
@@ -8,11 +8,13 @@ sudo yum remove docker \
                   docker-engine
 
 sudo yum install -y yum-utils
+
 sudo yum-config-manager \
     --add-repo \
     https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install docker-ce docker-ce-cli containerd.io
+sudo yum install docker-ce docker-ce-cli containerd.io -y
 
 sudo systemctl enable docker
+
 sudo systemctl start docker
