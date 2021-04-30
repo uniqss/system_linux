c4040a3e2496f1560c0e2b81855f3db6045e02b5
uniqs <uniqs@163.com>
2021-04-30 11:47:10 +0800

    dd

--- shell/docker_install.sh
+++ shell/docker_install.sh
@@ -1,8 +1,8 @@
 
-docker run --name redis -p 6379:6379 -d redis 
-docker run --name mongodb -p 27017:27017 -d mongo 
-docker run --name etcd -d -p 127.0.0.1:2379-2380:2379-2380 --env ALLOW_NONE_AUTHENTICATION=yes bitnami/etcd
-docker run --name nats-main -p 4222:4222 -p 6222:6222 -p 8222:8222 nats
+docker run -d --name redis -p 6379:6379 redis 
+docker run -d --name mongodb -p 27017:27017 mongo 
+docker run -d --name etcd -p 127.0.0.1:2379-2380:2379-2380 --env ALLOW_NONE_AUTHENTICATION=yes bitnami/etcd
+docker run -d --name nats-main -p 4222:4222 -p 6222:6222 -p 8222:8222 nats
 
 
 sudo firewall-cmd --add-port=6379/tcp --zone=public --permanent
