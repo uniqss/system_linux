81b692848ec34bd198db33168570848d1c05e2fc
uniqs <uniqs@163.com>
2021-04-30 13:47:39 +0800

    repo

---
+++ shell/centos8_repo.sh
@@ -0,0 +1,22 @@
+
+sudo mkdir -p /etc/yum.repos.d.bk/
+
+sudo cp -r /etc/yum.repos.d/* /etc/yum.repos.d.bk/
+
+
+sudo sed -e 's|^mirrorlist=|#mirrorlist=|g' \
+         -e 's|^#baseurl=http://mirror.centos.org/$contentdir|baseurl=https://mirrors.ustc.edu.cn/centos|g' \
+         -i.bak \
+         /etc/yum.repos.d/CentOS-Linux-AppStream.repo \
+         /etc/yum.repos.d/CentOS-Linux-BaseOS.repo \
+         /etc/yum.repos.d/CentOS-Linux-Extras.repo \
+         /etc/yum.repos.d/CentOS-Linux-PowerTools.repo \
+         /etc/yum.repos.d/CentOS-Linux-Plus.repo
+
+
+sudo yum install -y epel-release
+sudo sed -e 's|^metalink=|#metalink=|g' \
+         -e 's|^#baseurl=https\?://download.fedoraproject.org/pub/epel/|baseurl=https://mirrors.ustc.edu.cn/epel/|g' \
+         -i.bak \
+         /etc/yum.repos.d/epel.repo
+
