0f79f795356414e97995f2625f5db4dc6d420dcd
uniqss <uniqs@163.com>
2021-04-30 22:44:53 +0800

    go install

---
+++ shell/go_install.sh
@@ -0,0 +1,14 @@
+
+wget https://gomirrors.org/dl/go/go1.16.3.linux-amd64.tar.gz
+
+
+rm -rf /usr/local/go
+
+
+tar -C /usr/local -xzf go1.16.3.linux-amd64.tar.gz
+
+
+#vim /etc/profile
+#export PATH=$PATH:/usr/local/go/bin
+
+
