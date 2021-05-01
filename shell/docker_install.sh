539e53c9ab1994d398ac1488e6750c544affe351
uniqss <uniqs@163.com>
2021-05-01 11:12:21 +0800

    xx

--- shell/docker_install.sh
+++ shell/docker_install.sh
@@ -1,8 +1,8 @@
 
-docker run -d --restart --name redis -p 6379:6379 redis 
-docker run -d --restart --name mongodb -p 27017:27017 mongo 
-docker run -d --restart --name etcd -p 2379-2380:2379-2380 --env ALLOW_NONE_AUTHENTICATION=yes bitnami/etcd
-docker run -d --restart --name nats-main -p 4222:4222 -p 6222:6222 -p 8222:8222 nats
+docker run -d --restart unless-stopped --name redis -p 6379:6379 redis 
+docker run -d --restart unless-stopped --name mongodb -p 27017:27017 mongo 
+docker run -d --restart unless-stopped --name etcd -p 2379-2380:2379-2380 --env ALLOW_NONE_AUTHENTICATION=yes bitnami/etcd
+docker run -d --restart unless-stopped --name nats-main -p 4222:4222 -p 6222:6222 -p 8222:8222 nats
 
 
 sudo firewall-cmd --add-port=6379/tcp --zone=public --permanent
